e3ba815cefd8db0bff50532ad06e74dda03354b3
Roman Weich <roman.weich@gmail.com>
2021-08-23 22:45:56 +0200

    fix: matchdata schema for openligadb
    one of the values can be null but was not declared so

--- test/openligadb/MatchDataTypeTest.ts
+++ test/openligadb/MatchDataTypeTest.ts
@@ -1,11 +1,14 @@
-import { expect, use } from 'chai';
-import jsonschema from 'chai-json-schema';
 import 'mocha';
+
+import { expect, use } from 'chai';
+
 import { MatchDataType } from '../../src/api/openligadb/types/MatchDataType';
+import jsonschema from 'chai-json-schema';
 import matchdataBeforeStart from './fixtures/matchdata.beforestart.json';
 import matchdataFinished from './fixtures/matchdata.finished.json';
 import matchdataFinishedPenalty from './fixtures/matchdata.finnished.penalty-shootout.json';
 import matchdataRunningNoGoals from './fixtures/matchdata.running.nogoals.json';
+import matchdataRunningNoResultDesc from './fixtures/matchdata.running.nogoals.noresultdesc.json';
 import matchdataRunningOneGoal from './fixtures/matchdata.running.onegoal.json';
 
 use(jsonschema);
@@ -25,5 +28,8 @@ describe('MatchDataType', () => {
   });
   it('should validate against a finished match that had a penalty shootout', () => {
     expect(matchdataFinishedPenalty).to.be.jsonSchema(MatchDataType);
+  });
+  it('should validate even if the result description is null', () => {
+    expect(matchdataRunningNoResultDesc).to.be.jsonSchema(MatchDataType);
   });
 });
